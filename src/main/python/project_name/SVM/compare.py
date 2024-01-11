26e28d9072db731896babceacd7c681df5ac2afe
Renato <renatopjesus1818@gmail.com>
2024-01-11 18:52:38 +0100

    refactor main

--- src/main/python/project_name/SVM/compare.py
+++ src/main/python/project_name/SVM/compare.py
@@ -2,11 +2,65 @@ import cv2
 import sqlite3
 import numpy as np
 from tqdm import tqdm
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import SVM.sift as sift
+import SVM.sufr as surf
+import DataBase.database as db
+from SVM import compare as cp
+from SVM import sift
+from tqdm import tqdm
 
 class Compare:
     def __init__(self, sift_comparer):
         self.sift_comparer = sift_comparer
 
+    def compare (self, database, new_image_path):            
+
+        # Read image paths from the database
+        types, database_image_paths = database.read_images()
+        if not database_image_paths: 
+            return -1
+
+        # Compare the new image with images from the database
+        results = self.compare_with_database(new_image_path, database_image_paths)
+
+        # Scope for each type of flowers
+        types_with_scores = self.find_scope(types, results)        
+        print(types_with_scores)
+
+        # The best type
+        most_similar_type = self.best_scope(types_with_scores)
+        print(most_similar_type)
+
+
+        # Display the most similar image
+        i=0
+        most_similar_path, most_similar_folder, most_similarity_score = results[i]
+        for database_image_path, folder_name, similarity_score in results:
+            if similarity_score > most_similarity_score:
+                most_similar_path, most_similar_folder, most_similarity_score = results[i]
+                print(most_similarity_score)
+            i=i+1
+                
+        most_similar_img = cv2.imread(most_similar_path, cv2.IMREAD_COLOR)
+
+        print(f"Number of analise images: {len(results)}" )
+        print(f"Most similar folder: {most_similar_folder}" )
+
+        # Plot the images 
+        fig, axs = plt.subplots(1, 2, figsize=(10, 5))
+        axs[0].imshow(cv2.cvtColor(cv2.imread(new_image_path, cv2.IMREAD_COLOR), cv2.COLOR_BGR2RGB))
+        axs[0].set_title("Image to classify")
+        axs[0].axis("off")
+        axs[1].imshow(cv2.cvtColor(most_similar_img, cv2.COLOR_BGR2RGB))
+        axs[1].set_title(f"Most Similar Image\nType: {most_similar_folder}\nScore: {most_similarity_score}")
+        axs[1].axis("off")
+        plt.show()
+
+        sift.compare_images_sift(new_image_path, most_similar_path)
+    
     def compare_with_database(self, new_image_path, database_image_paths):
         """
         Compares the new image with each image in the database.
@@ -28,10 +82,6 @@ class Compare:
             # Append the result to the list
             similarity_scores.append((database_image_path, folder_name, similarity_score))
 
-        """
-        total_sum = 0
-        total_points = len(selected_image), total_sum, folder_name
-        """
         return similarity_scores
     
     def find_scope(self, types, results):        
--- src/main/python/project_name/main.py
+++ src/main/python/project_name/main.py
@@ -23,67 +23,24 @@ test_figure_path2 = "C:/Users/asus/GitHub_clones/DIP23/src/main/python/project_n
 #sift.compare_sum_of_images_with_external_figure(image1_path, image2_path, test_figure_path)
 #surf.compare_images_surf(image1_path, image2_path)
 
-
+#### ASK DATABASE ####
 # Set the paths for the database
-database_path = askdirectory()
+'''database_path = askdirectory()
 if not database_path:
     print("No directory selected. Exiting program.")
-    sys.exit()
+    sys.exit()'''
+database_path="C:/Users/asus/GitHub_clones/DIP23/src/resources/data_base"
 
+#### ASK IMAGE TEST ####
 #new_image_path = "path/to/new_image.jpg"
 new_image_path = test_figure_path2
 
 # Initialize classes
 database = db.Database(database_path)
-sift_comparer = sift.Sift()
-image_comparer = cp.Compare(sift_comparer)
+#sift_comparer = sift.Sift()
+image_comparer = cp.Compare(sift.Sift())
 
-# Read image paths from the database
-types, database_image_paths = database.read_images()
-if not database_image_paths: 
-    print("No images found. Exiting program.")
+if image_comparer.compare(database, new_image_path) == -1:     
+    print("Error")
     sys.exit()
-
-# Compare the new image with images from the database
-results = image_comparer.compare_with_database(new_image_path, database_image_paths)
-
-# Print the results
-"""aux=0
-for database_image_path, similarity_score in results:
-    print(f"Image: {aux}, Similarity Score: {similarity_score} \n")
-    aux=aux+1"""
-
-# Scope for each type of flowers
-types_with_scores = image_comparer.find_scope(types, results)        
-print(types_with_scores)
-
-# The best type
-most_similar_type = image_comparer.best_scope(types_with_scores)
-print(most_similar_type)
-
-
-# Display the most similar image
-i=0
-most_similar_path, most_similar_folder, most_similarity_score = results[i]
-for database_image_path, folder_name, similarity_score in results:
-    if similarity_score > most_similarity_score:
-        most_similar_path, most_similar_folder, most_similarity_score = results[i]
-        print(most_similarity_score)
-    i=i+1
-        
-most_similar_img = cv2.imread(most_similar_path, cv2.IMREAD_COLOR)
-
-print(f"Number of analise images: {len(results)}" )
-print(f"Most similar folder: {most_similar_folder}" )
-
-# Plot the images 
-fig, axs = plt.subplots(1, 2, figsize=(10, 5))
-axs[0].imshow(cv2.cvtColor(cv2.imread(new_image_path, cv2.IMREAD_COLOR), cv2.COLOR_BGR2RGB))
-axs[0].set_title("Image to classify")
-axs[0].axis("off")
-axs[1].imshow(cv2.cvtColor(most_similar_img, cv2.COLOR_BGR2RGB))
-axs[1].set_title(f"Most Similar Image\nType: {most_similar_folder}\nScore: {most_similarity_score}")
-axs[1].axis("off")
-plt.show()
-
-sift.compare_images_sift(new_image_path, most_similar_path)
+    
